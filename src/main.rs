87b0c6869c1030898613a68a804d2cec2ce1c576
Ricardo Arias <reariast128@proton.me>
2024-01-01 18:04:28 -0500

    PORQUE NO ME DEJA COMPILAR EL PROYETO AAAAAAAAAAAAAA

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
 fn main() {
-    println!("Por favor, introduce tu nombre: ");
-    let mut name: String = String::new();
-    std::io::stdin().read_line(&mut name).unwrap();
-    name = name.trim().to_string();
+    let n1: i32 = 123;
+    let n2: i32 = 123;
+    let sum: i32 = n1 + n2;
 
-    println!("Ahora, introduce tu edad");
-    let mut age: String = String::new();
-    std::io::stdin().read_line(&mut age).unwrap();
-
-    let parsed_age: u8 = age.trim().parse().unwrap();
-
-    println!("OK. Entonces, tu nombre es: {}, y tu edad es de {}.", name, parsed_age);
-    if parsed_age >= 18{
-        println!("Hey! Eres mayor de edad.")
-    } else if parsed_age == 17 || parsed_age == 16{
-        println!("Ya casi eres mayor!")
-    } else {
-        println!("Hmmmm, eres menor de edad.")
-    }
+    println!("Por favor, escribe la suma de {} y {}.", n1, n2)
 }
