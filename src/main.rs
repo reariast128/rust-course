0a23789003db706ade65b2c60f4992923f8bbfc9
Ricardo Arias <reariast128@proton.me>
2024-01-01 11:55:29 -0500

    Finished first lesson.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Muy buenas tardes compañeros");
+    let age: u8 = 18;
+    let _unused: u8;
+    let name: &str = "Creeping";
+    let mut money: u8 = 50;
+    println!("Hola soy {} y tengo {} años, y {} pesos", name, age, money);
+
+    money += 30;
+    println!("Ahora tengo {} pesos", money)
 }
