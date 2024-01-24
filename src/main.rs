54d6084ce0f89dd091a64314378df9568d0177b5
Ricardo Arias <reariast128@proton.me>
2024-01-23 23:14:37 -0500

    Started the game project!

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str = "history.csv";
+
+fn read_game(mut game_content: csv::Reader<&[u8]>) {
+    for record in game_content.records() {
+        println!("Texto: {}", record.unwrap().get(2).unwrap().trim());
+    };
+}
+
 fn main() {
-    let mut names: Vec<String> = Vec::new();
-
-    for _ in 1..4 {
-        println!("Por favor, introduce un nombre:");
-        let mut name = String::new();
-        std::io::stdin().read_line(&mut name).unwrap();
-
-        names.push(name.to_string())
+    let raw_content: Result<String, std::io::Error> = fs::read_to_string(FILENAME);
+    let reader_builder: csv::Reader<&[u8]>;
+    match raw_content {
+        Ok(file_reader) => {
+            reader_builder = ReaderBuilder::new().delimiter(b';').from_reader(file_reader.as_bytes());
+            read_game(reader_builder);
+        },
+        Err(error) => {
+            println!("Ha ocurrido un error: {}", error);
+        }
     }
 
-    for names_iter in names {
-        println!("El nombre es {}", names_iter);
-    }
+    
+    
 }
