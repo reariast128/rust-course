85e79464b7862e864e7a87ab3b2f4fd0e10b7620
Ricardo Arias <reariast128@proton.me>
2024-01-23 23:19:30 -0500

    Formated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use csv::{ReaderBuilder, StringRecord};
-use std::{fs};
+use csv::{ ReaderBuilder, StringRecord };
+use std::{ fs };
 
 const FILENAME: &str = "history.csv";
 
 fn read_game(mut game_content: csv::Reader<&[u8]>) {
     for record in game_content.records() {
         println!("Texto: {}", record.unwrap().get(2).unwrap().trim());
-    };
+    }
 }
 
 fn main() {
@@ -14,14 +14,13 @@ fn main() {
     let reader_builder: csv::Reader<&[u8]>;
     match raw_content {
         Ok(file_reader) => {
-            reader_builder = ReaderBuilder::new().delimiter(b';').from_reader(file_reader.as_bytes());
+            reader_builder = ReaderBuilder::new()
+                .delimiter(b';')
+                .from_reader(file_reader.as_bytes());
             read_game(reader_builder);
-        },
+        }
         Err(error) => {
             println!("Ha ocurrido un error: {}", error);
         }
     }
-
-    
-    
 }
